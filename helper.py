756df224e709b49c356d19d77f708375c97ed442
Niusha <niushamirhakimi@gmail.com>
2023-08-03 15:43:04 -0400

    bootstrap and permutation functions were added

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -5,7 +6,7 @@ subject_group = "dx_group"
 
 #you should make sure there is no empty cell in the target column
 
-def get_data(path:Path, target_column: str, target_groups:list):
+def get_data(path:Path, target_column: str, target_groups:list) -> tuple:
 
     df = pd.read_csv(path)
     data_list = []
@@ -14,5 +15,34 @@ def get_data(path:Path, target_column: str, target_groups:list):
 
     return tuple(data_list)
 
+def bootstrapped_cohorts(data:list, n:int=1000) -> list:
+    
+    cohort_list = []
+   
+    for group in data:
+        bootstrapped = np.zeros((n,len(group)))
+        for i in range(n):
+            bootstrapped[i] = np.random.choice(group, len(group), replace=True)
+        
+        cohort_list.append(bootstrapped)
 
+    return cohort_list
+
+
+def permutated_cohorts(data:tuple, n:int=1000) -> list:
+
+    lengths = [len(group) for group in data]
+    merged_data = tuple(itertools.chain(*data))
+
+    permuted_cohorts = []
+    for length in lengths:
+        permuted_cohorts.append(np.zeros((n, length)))
+
+    for i in range(n):
+           permuted = np.random.permutation(merged_data, len(merged_data), replace=False)
+           for (j, length) in enumerate(lengths):
+               permuted_cohorts[j][i] = permuted[:length]
+               permuted = permuted[length:]
+
+    return permuted_cohorts
 
