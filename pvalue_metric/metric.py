abff238eeeb3603cb6728f692c5a77d3d6da5e51
Niusha <niushamirhakimi@gmail.com>
2023-08-05 00:35:17 -0400

    add pvalue_test function_

--- pvalue_metric/metric.py
+++ pvalue_metric/metric.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from pvalue_metric import helper
 
 def mean_euclidean_distance(pvalues:np.array):
 
@@ -18,5 +18,40 @@ def CDF_test(deltas, original_delta):
 
     return 1 - delta_CDF[threshold_index]
 
+def pvalue_test(data, Hypothesis_testing_func, n_bootstrap, n_permutation, **kwargs):
 
+    G1_permutations, G2_permutations = helper.permutated_cohorts(data, n_permutation)
 
+    mean_deltas = np.zeros((n_permutation,))
+    for permutation_itr in range(n_permutation):
+        G1_bootstraps, G2_bootstraps = helper.bootstrapped_cohorts([G1_permutations[permutation_itr], G2_permutations[permutation_itr]], n_bootstrap)
+        p_values = np.zeros((n_bootstrap,))
+
+        for bootstrap_itr in range(n_bootstrap):
+            p_values[bootstrap_itr] = Hypothesis_testing_func(G1_bootstraps[bootstrap_itr],\
+                                                G2_bootstraps[bootstrap_itr], **kwargs)[1]
+
+        mean_deltas[permutation_itr] = mean_euclidean_distance(p_values)
+
+    original_cohort_G1_bootstrapes, original_cohort_G2_bootstrapes = \
+        helper.bootstrapped_cohorts(data, n_bootstrap)
+     
+    original_cohort_pvalues = [Hypothesis_testing_func(original_cohort_G1_bootstrapes[i],\
+                                original_cohort_G2_bootstrapes[i], **kwargs)[1] for i in range(n_bootstrap)]
+
+    original_mean_delta = mean_euclidean_distance(original_cohort_pvalues)
+    
+    return CDF_test(mean_deltas, original_mean_delta)
+
+    # n1 = len(G1)
+    # n2 = len(G2)
+    # n = n1 + n2
+    # G = np.concatenate((G1, G2))
+    # delta = Hypothesis_testing_function(G, **kwargs)
+    # deltas = np.zeros((n_bootstrap,))
+    # for i in range(n_bootstrap):
+    #     G1_bootstrap = np.random.choice(G, n1, replace=True)
+    #     G2_bootstrap = np.random.choice(G, n2, replace=True)
+    #     deltas[i] = Hypothesis_testing_function(np.concatenate((G1_bootstrap, G2_bootstrap)), **kwargs)
+    
+    # return CDF_test(deltas, delta)
