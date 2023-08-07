9ffc070f822295d5e8a20ac5ba1c698a7f0a4a9a
Niusha <niushamirhakimi@gmail.com>
2023-08-07 14:52:35 -0400

    first version of pvalue_test_for_all_volumes

--- analysis/main.py
+++ analysis/main.py
@@ -4,7 +4,10 @@ from scipy import stats
 from pathlib import Path
 from pvlaue_metric import metric
 
-def test_for_all_volumes(path:Path,
+
+group_column = "dx_group"
+
+def pvalue_test_for_all_volumes(path:Path,
                         regions:list(str),
                         groups:list,
                         n_bootstrap:int,
@@ -17,9 +20,21 @@ def test_for_all_volumes(path:Path,
         print("Only two groups are allowed for now")
         raise
 
-    data = pd.dataframe(path)    
-    for region in regions:
-        tests_outputs, _ = metric.pvalue_test(data, n_bootstrap, n_permutation, **kwargs)
-        pvalue, _, _ = tests_outputs
-        
+    data = pd.dataframe(path)
+    results = np.zeros((len(regions),)) 
+       
+    for (i, region) in enumerate(regions):
+
+        data_columns = []
+        for group in groups:
+            data_columns.append(data[region].loc[data[group_column] == group])
+
+        data_columns = tuple(data_columns)
+    
+        tests_outputs, _ = metric.pvalue_test(data_columns, n_bootstrap, n_permutation, **kwargs)
+        results[i], _, _ = tests_outputs
+        # pandas.Series(data=None, index=None, dtype=None, name=None, copy=None, fastpath=False)[source]
+
+    return pd.Series(results, index=regions, name= group[0] + "_vs_" + group[1]))    #maybe change name  
+
 
