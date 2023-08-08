9b141e619a44aea993641d2aa6b319ae91328bfb
Niusha <niushamirhakimi@gmail.com>
2023-08-07 23:55:05 -0400

    all 3 groups' results

--- analysis/main.py
+++ analysis/main.py
@@ -6,7 +6,7 @@ from pvalue_metric import metric
 from itertools import combinations
 
 
-group_column = "dx_group"
+group_column = "dx_group" ##REMOVE IT FROM HERE there are copies in helper.py and main.py
 # Hypothesis_test:callable=stats.ttest_ind,
 def pvalue_test_for_all_volumes(path:Path,
                         regions:list,
@@ -39,5 +39,29 @@ def pvalue_test_for_all_volumes(path:Path,
     return pd.Series(results, index=regions, name= groups[0] + "_vs_" + groups[1])    #maybe change name  
 
 def generate_pairs_indices(data):
-    return list(combinations(range(len(data)), 2))
 
+    pairs_elements = []
+    for i, j in combinations(data, 2):
+        pairs_elements.append((i, j))
+
+    return pairs_elements
+
+PARENT_DIR = Path(__file__).resolve().parents[1]
+Data_DIR = PARENT_DIR / "study_files/roi_data.csv"
+
+df = pd.read_csv(Data_DIR)
+target_groups = ["PD-non-MCI", "HC"]
+subject_group = "dx_group"
+ROIs = [
+"Thalamus", "Caudate", "Putamen", "Pallidum",
+"Hippocampus", "Amygdala","Accumbens-area"]
+target_columns = ["thickness_change"] + [region+"_change_pred" for region in ROIs]
+
+pairs = generate_pairs_indices(df[subject_group].unique())
+print(pairs)
+result = []
+for pair in pairs:
+
+    result.append(pvalue_test_for_all_volumes(Data_DIR, target_columns, pair, stats.ttest_ind, 500, 500))
+
+print(pd.concat(result, axis=1))
