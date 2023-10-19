175d25dc45ce6f587ea650a6aa628c4c43c53e72
Niusha <niushamirhakimi@gmail.com>
2023-10-19 13:51:09 -0400

    modify minor path issues to work with container

--- simulations/normal_simulation_different_effectsize_samplesize.py
+++ simulations/normal_simulation_different_effectsize_samplesize.py
@@ -5,12 +5,15 @@ from pathlib import Path
 from scipy import stats
 from simulation_helper import simulation
 
-# TASK_ID = int(os.environ.get('TASK_ID'))
-# INPUTFILENAME = os.environ.get('INPUTFILENAME')
-cwd = Path(__file__).parent.absolute()
+TASK_ID = int(os.environ.get('TASK_ID'))
+INPUTFILENAME = os.environ.get('INPUTFILENAME')
 
-TASK_ID = 1
-INPUTFILENAME = "simulation_combinations.csv"
+cwd = Path(__file__).parent.absolute()
+output_dir = Path("/mnt/output/results")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# TASK_ID = 1
+# INPUTFILENAME = "simulation_combinations.csv"
 
 pmetrics = np.zeros(1000)
 pvalues = np.zeros(1000)
@@ -21,7 +24,7 @@ combinations_df.astype({'size': 'int64', 'n_bootstrap': 'int64', 'n_permutation'
 
 for i in range(1):
 
-    combination = combinations_df.loc[TASK_ID] # it doesnt work with correct types
+    combination = combinations_df.loc[TASK_ID] # it doesnt work with correct data types
 
     func1_args = {'loc': 0, 'scale': 1, 'size': combinations_df['size'][TASK_ID]}
     func2_args = {'loc':  combinations_df['locations'][TASK_ID], \
@@ -34,4 +37,4 @@ for i in range(1):
     pvalues[i] = simulation_result['pvalue']
 
 result = pd.DataFrame({'p_metric': pmetrics, 'pvalue': pvalues})
-result.to_csv(cwd / f'simulation_result_{TASK_ID}.csv')
+result.to_csv(output_dir / f'simulation_result_{TASK_ID}.csv') #Save the result in job directory not in simulation code directory
--- simulations/pmetric_job.sh
+++ simulations/pmetric_job.sh
@@ -4,16 +4,11 @@
 #$ -t 1-50:1
 #$ -cwd
 #$ -S /bin/bash
-#$ -o "/data/origami/niusha/$JOB_NAME/out/$JOB_NAME_$TASK_ID.out"
-#$ -e "/data/origami/niusha/$JOB_NAME/err/$JOB_NAME_$TASK_ID.err"
+#$ -o "/data/origami/niusha/$JOB_NAME/out/$JOB_NAME_$SGE_TASK_ID.out"
+#$ -e "/data/origami/niusha/$JOB_NAME/err/$JOB_NAME_$SGE_TASK_ID.err"
 
-while getops ":s:i" opt; do
-    case $opt in
-        s) script="$OPTARG"
-        ;;
-        i) INPUTFILENAME="$OPTARG"
-        ;;
-        \?) echo "Invalid option -$OPTRAG" >&2 # change it according to container stuff
+script=$1
+INPUTFILENAME=$2
 
 home_dir="/data/origami/niusha"
 PARENT_DIR="${home_dir}/$JOB_NAME"
@@ -23,29 +18,11 @@ then
     echo "Parent directory does not exist"
 fi
 
-# line_number=$(echo "$SGE_TASK_ID + 1" | bc)
-# line=$(cut -f $SGE_TASK_ID -d $'\n' $INPUTFILE)
-
-DESTINATION_DIR="${PARENT_DIR}/test-$SGE_TASK_ID" #change this to the name of the folder you want to save the outputs in
-
-if [ ! -d "$DESTINATION_DIR" ]
-then 
-    mkdir -p $DESTINATION_DIR
-fi
-
-# cp $INPUTFILENAME $DESTINATION_DIR
-cd $DESTINATION_DIR
-
 #Run the program
 export SGE_TASK_ID
-export line
-
+export INPUTFILENAME
 
 singularity exec -e --bind ${home_dir}/code/pmetric:/mnt/code \
---bind ${home_dir}/pmetric_input:/mnt/input:ro \
---bind ${home_dir}/pemetric_outputs:/mnt/out \
-${home_dir}/pmetric_python.sif \
-python /mnt/code/${script} $INPUTFILENAME $TASK_ID
-
-# normal_simulation_different_effectsize_samplesize.py 
-
+            --bind $PARENT_DIR:/mnt/out \
+            ${home_dir}/pmetric_python.sif \
+            python3 /mnt/code/${script} 
